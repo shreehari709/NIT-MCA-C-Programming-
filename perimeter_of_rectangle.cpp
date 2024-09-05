021448f9aaf7d8744c895cf290011586ccf04e08
SHREEHARI KALUNDIA <101623053+shreehari709@users.noreply.github.com>
2024-09-05 19:24:50 +0530

    Update perimeter_of_rectangle.cpp

--- perimeter_of_rectangle.cpp
+++ perimeter_of_rectangle.cpp
@@ -1,11 +1,9 @@
 //Write a C program to compute the perimeter and area of a rectangle with a height of 7 inches and width of 5 inches.
 #include<stdio.h>
 int main(){
-	int l=7;
-	int b=5;
-	int a,p;
-	a=l*b;
-	p=2*(l+b);
-	printf("Area of Rectangle = %d and \nPerimeter of Retangle = %d\n",a,p);
+	int height=7, width=5;
+	int area=height*width;
+	int perimeter=2*(height+width);
+	printf("Area of Rectangle = %d and \nPerimeter of Retangle = %d\n",area,perimeter);
 	return 0;
 }
